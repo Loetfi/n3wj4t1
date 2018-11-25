2ef3ecd2f215809d94b893140bba7fb6f71e726f
Ahmad Djunaedi <ahmaddjunaedi92@gmail.com>
2018-11-25 22:30:50 +0700

    add function dd (dump and die)

--- application/helpers/lutfi_helper.php
+++ application/helpers/lutfi_helper.php
@@ -73,4 +73,25 @@ function getCetak($id='')
 	$query = $ci->db->query("SELECT * from msattrcetak where idattributecetak = '$id'  ")->row_array(); 
 	return $query['attributecetak'];
 }
+
+// dump and die
+function dd() {
+	$args = func_get_args();
+    ob_start();
+    echo '<pre>';
+    foreach($args as $vars){
+        if(is_bool($vars) || is_null($vars))
+            var_dump($vars);
+        else
+            echo htmlspecialchars(print_r($vars, true), ENT_QUOTES);
+        echo PHP_EOL;
+    }
+    echo '</pre>';
+    
+    $ctx = ob_get_contents();
+    ob_end_clean();
+    
+    echo $ctx;
+    die;
+}
 ?>
