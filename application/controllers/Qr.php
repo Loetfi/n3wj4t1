f9842ef5b322bffbab03ac494a1411317bab42a6
Tupay <lutfi@mhs.ubharajaya.ac.id>
2018-12-05 19:41:40 +0700

    add QR Code lib new

--- application/controllers/Qr.php
+++ application/controllers/Qr.php
@@ -1,7 +1,8 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-use Endroid\QrCode\QrCode;
+// use Endroid\QrCode\QrCode;
+require_once APPPATH.'../phpqrcode/qrlib.php';
 
 class Qr extends CI_Controller {
 
@@ -10,14 +11,28 @@ class Qr extends CI_Controller {
 
 	public function index()
 	{
+		// include('../lib/full/qrlib.php');
+		$PNG_TEMP_DIR = dirname(__FILE__).DIRECTORY_SEPARATOR.'../../qrcode'.DIRECTORY_SEPARATOR;
+		// die();
+
+    // outputs image directly into browser, as PNG stream
+		QRcode::png('1000'.rand(0,1000), $PNG_TEMP_DIR.'test3.png', 'L', 4, 2);
+
+		// $tempDir = APPPATH.'../qrcode';
+		// file_get_contents('oke.png', QRcode::png('PHP QR Code :)'));
+
+		die();
+
+		$codeContents = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Proin nibh augue, suscipit a'; 
+		QRcode::png($codeContents, $tempDir.'006_L1.png', QR_ECLEVEL_L); 
 		// require FCPATH . 'vendor/autoload.php';
 		// include APPPATH.'../vendor/autoload.php';
 		// die();
-		$params = $this->input->get('params') ? $this->input->get('params') : 0;
-		$qrCode = new QrCode($params);
+		// $params = $this->input->get('params') ? $this->input->get('params') : 0;
+		// $qrCode = new QrCode($params);
 
-		header('Content-Type: '.$qrCode->getContentType());
-		echo $qrCode->writeString();
+		// header('Content-Type: '.$qrCode->getContentType());
+		// echo $qrCode->writeString();
 		
 	}
 
